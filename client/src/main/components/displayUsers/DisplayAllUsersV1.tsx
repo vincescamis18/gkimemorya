137813d3cfcfcd5b69e40d8e45182c4173dfff9f
Jude Jawod <Jawod50@gmail.com>
2022-04-17 17:19:37 +0800

    Redirect user to own profile when it clicks itself

--- client/src/main/components/displayUsers/DisplayAllUsersV1.tsx
+++ client/src/main/components/displayUsers/DisplayAllUsersV1.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 import axios from "axios";
+import { RootState } from "../../redux/reducers/allReducer";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const DisplayAllMemoryUsersV1 = () => {
 	const navigate = useNavigate();
 	const [allUsers, setallUsers] = useState([]);
+	const userState = useSelector((state: RootState) => state.user);
 
 	useEffect(() => {
 		axios
@@ -17,7 +20,14 @@ const DisplayAllMemoryUsersV1 = () => {
 	return (
 		<div className="center-all-users-parent">
 			{allUsers?.map((user: any, index: number) => (
-				<div className="user-item no-select" key={index} onClick={() => navigate(`/profile/${user._id}`)}>
+				<div
+					className="user-item no-select"
+					key={index}
+					onClick={() => {
+						if (user._id === userState._id) navigate("/profile");
+						else navigate(`/profile/${user._id}`);
+					}}
+				>
 					<div className="user-details-container ">
 						<img src={user.user_profile} alt="profile" className="user-image-container" />
 						<h1>{user.given_name + " " + user.surname}</h1>
